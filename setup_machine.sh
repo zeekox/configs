bbbf4cd6c98e4c5f93ea96111a55f66c9ca8e2a2
Java Developer <neo@ubuntu.(none)>
2014-03-07 22:19:25 +0100

    script to configure new machine with my configs

---
+++ setup_machine.sh
@@ -0,0 +1,17 @@
+
+sudo apt-get install awesome awesome-extra vim-gnome git ctags rxvt-unicode-256color gmrun
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "taking files from "$DIR
+
+cd
+ln -s $DIR/.vim
+ln -s $DIR/.vimrc
+ln -s $DIR/.Xresources
+xrdb .Xresources
+cd .config
+mkdir awesome
+cd awesome
+ln -s $DIR/rc.lua
+ln -s $DIR/vicious
+ln -s $DIR/widget_fun.lua
+ln -s $DIR/widgets.lua
