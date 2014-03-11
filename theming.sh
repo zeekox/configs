c81f40d1fa6e9a6d23dff80ac73d9b95d29fd6e3
Mauro Carabotti <carabotti@puzzle.ch>
2014-03-11 15:27:32 +0100

    updated theming

---
+++ theming.sh
@@ -0,0 +1,6 @@
+# wget http://killhellokitty.deviantart.com/art/Delorean-dark-eclipse-3-8-390040014
+# wget http://killhellokitty.deviantart.com/art/DeLorean-Dark-3-10-ALPHA-5-03102014-408652063
+unzip delorean_dark_eclipse_3_8_by_killhellokitty-d6g7wsu.zip 
+ cd data/Downloads/
+ cp DeLorean-Dark-3.8_1.0.0.201308011234.jar /neo/apps/eclipse/dropins/
+ cp images/* /neo/apps/eclipse/plugins/org.eclipse.platform_4.3.1.v20130911-1000/images/
