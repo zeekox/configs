f4aed0317f36562a21efdb07d38ae6160c64e39d
Mauro Carabotti <carabotti@puzzle.ch>
2015-05-04 13:56:50 +0200

    xrandr config with new mini laptop

--- xrandr.sh
+++ xrandr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-dp0=`xrandr -q | grep "DP-0 disconnected" > /dev/null ; echo $?`
-dp2=`xrandr -q | grep "DP-2 disconnected" > /dev/null ; echo $?`
-dp6=`xrandr -q | grep "DP-6 disconnected" > /dev/null ; echo $?`
+dp0=`xrandr -q | grep "DP1 disconnected" > /dev/null ; echo $?`
+dp2=`xrandr -q | grep "HDMI1 disconnected" > /dev/null ; echo $?`
+dp6=`xrandr -q | grep "eDP1 disconnected" > /dev/null ; echo $?`
 
 
- xrandr --output LVDS1 --auto --pos 0x0 --output DP-0 --primary --auto --rotate normal --right-of LVDS1 --output DP-2 --auto --rotate normal --right-of DP-0
+ xrandr --output eDP1 --auto --pos 0x0 --output DP1 --primary --auto --rotate normal --right-of eDP1 --output HDMI1 --auto --rotate normal --right-of DP1
