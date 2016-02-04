50176cc52e0675ebef549633fd8defb4cbbfe2a1
Mauro Carabotti <carabotti@puzzle.ch>
2016-02-04 15:56:43 -0500

    add backup script and script go set default browser

---
+++ utils/set_default_browser.sh
@@ -0,0 +1,3 @@
+sudo update-alternatives --config x-www-browser
+xdg-mime default chromium-browser-chromium.desktop x-scheme-handler/https
+xdg-mime default chromium-browser-chromium.desktop x-scheme-handler/http
